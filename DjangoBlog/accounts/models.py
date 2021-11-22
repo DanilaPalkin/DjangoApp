058facc5bb78156fdd0bf915fe1246bf0d053b93
DanilaPalkin <danilapalkin@gmail.com>
2021-11-22 16:02:31 +0300

    Доработка модели и шаблона

--- DjangoBlog/accounts/models.py
+++ DjangoBlog/accounts/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
     title = models.CharField(max_length=200, null=True)
-    text = models.TextField(null=True)
-    media = models.ImageField(null=True)
+    text = models.TextField(null=True, blank=True)
+    media = models.ImageField(null=True, blank=True)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    class Meta:
+        ordering = ['-date_created', ]
 
     def __str__(self):
         return self.title
--- DjangoBlog/accounts/views.py
+++ DjangoBlog/accounts/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 
 def feed(request):
-    return render(request, 'accounts/dashboard.html')
+    posts = Post.objects.all()
+    return render(request, 'accounts/dashboard.html', {'posts': posts})
 
 
 def posts(request):
