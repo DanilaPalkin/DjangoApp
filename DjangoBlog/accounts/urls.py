b2174a5d90753bd45065332946784a81ff908cf0
DanilaPalkin <danilapalkin@gmail.com>
2021-11-26 17:26:50 +0300

    Авторизация и регистрация
    Пользователи могут регистрироваться и авторизовываться на сайте

--- DjangoBlog/accounts/urls.py
+++ DjangoBlog/accounts/urls.py
@@ -5,5 +5,10 @@ urlpatterns = [
     path('', views.feed),
     path('user/<str:pk>/', views.user),
     path('post/<str:pk>/', views.post),
+
+    path('register/', views.registerPage),
+    path('login/', views.loginPage),
+    path('logout/', views.logoutUser),
+
     path('create_post/', views.createPost),
 ]
--- DjangoBlog/accounts/views.py
+++ DjangoBlog/accounts/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 from .models import *
 from .forms import PostForm
 from .filters import PostFilter
@@ -20,6 +24,39 @@ def post(request, pk):
 def user(request, pk):
     posts = Post.objects.filter(author=pk)
     return render(request, 'accounts/dashboard.html', {'posts': posts})
+
+
+def registerPage(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Вы были успешно зарегистрированы!')
+            return redirect('/login')
+
+    context = {'form': form}
+    return render(request, 'accounts/register.html', context)
+
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Неправильный логин или пароль')
+    context = {}
+    return render(request, 'accounts/login.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('/login')
 
 
 def createPost(request):
